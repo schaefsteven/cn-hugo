240081ecebbf221cd37354605c2ca653f33b299e
steven <schaefsteven@gmail.com>
2023-04-07 13:29:51 -0500

    set up daily builds via this tutorial: https://www.netlify.com/blog/how-to-schedule-deploys-with-netlify/

---
+++ static/netlify/functions/scheduled-deploy.js
@@ -0,0 +1,22 @@
+const fetch = require('node-fetch')
+import { schedule } from '@netlify/functions'
+
+// This is sample build hook
+const BUILD_HOOK = 'https://api.netlify.com/build_hooks/64305f80fd33ef0994ae0437'
+
+// Schedule the handler function to run daily
+const handler = schedule('0 1 * * *', async () => {
+  await fetch(BUILD_HOOK, {
+    method: 'POST'
+  }).then((response) => {
+    console.log('Build hook response:', response.json())
+  })
+
+  return {
+    statusCode: 200
+  }
+}
+
+export {
+  handler
+}
