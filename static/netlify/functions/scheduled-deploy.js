beb5d6d8851aca210e0c89ef9b3e674bd28f50c8
steven <schaefsteven@gmail.com>
2023-04-09 17:17:08 -0500

    updated build fuction based on https://docs.netlify.com/functions/scheduled-functions/

--- static/netlify/functions/scheduled-deploy.js
+++ static/netlify/functions/scheduled-deploy.js
@@ -1,21 +1,22 @@
 const fetch = require('node-fetch')
-import { schedule } from '@netlify/functions'
+import { Handler, HandlerEvent, HandlerContext, schedule } from '@netlify/functions'
 
 // This is sample build hook
 const BUILD_HOOK = 'https://api.netlify.com/build_hooks/64305f80fd33ef0994ae0437'
 
-// Schedule the handler function to run daily
-const handler = schedule('* * * * *', async () => {
+const myHandler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
   await fetch(BUILD_HOOK, {
     method: 'POST'
   }).then((response) => {
     console.log('Build hook response:', response.json())
   })
 
-  return {
-    statusCode: 200
-  }
-}
+    return {
+        statusCode: 200,
+    };
+};
+
+const handler = schedule("* * * * *", myHandler)
 
 export {
   handler
