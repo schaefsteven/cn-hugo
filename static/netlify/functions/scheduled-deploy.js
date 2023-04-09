caf7debfaeb51be17b60b3717e998f39496d8bbc
steven <schaefsteven@gmail.com>
2023-04-09 15:15:07 -0500

    changed function to run every minute for testing.

--- static/netlify/functions/scheduled-deploy.js
+++ static/netlify/functions/scheduled-deploy.js
@@ -5,7 +5,7 @@ import { schedule } from '@netlify/functions'
 const BUILD_HOOK = 'https://api.netlify.com/build_hooks/64305f80fd33ef0994ae0437'
 
 // Schedule the handler function to run daily
-const handler = schedule('0 1 * * *', async () => {
+const handler = schedule('* * * * *', async () => {
   await fetch(BUILD_HOOK, {
     method: 'POST'
   }).then((response) => {
